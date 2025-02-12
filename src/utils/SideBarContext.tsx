cf57069e03d4b1fe84ed6d96c7fb3c558bd41bb9
jensku <jensku@stud.ntnu.no>
2025-02-12 16:31:23 +0100

    Create Note taking page

---
+++ src/utils/SideBarContext.tsx
@@ -0,0 +1,27 @@
+// SidebarContext.tsx
+import React, { createContext, useState, useContext, ReactNode } from "react";
+
+interface SidebarContextType {
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+
+export const SidebarProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return <SidebarContext.Provider value={{ isSidebarOpen, toggleSidebar }}>{children}</SidebarContext.Provider>;
+};
+
+export const useSidebar = (): SidebarContextType => {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider");
+  }
+  return context;
+};
